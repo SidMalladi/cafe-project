5cd6990a5757ce0ba9c3877b075fca5500748032
Siddharth Malladi <siddharthmalladi@Siddharths-MacBook-Air.local>
2025-05-24 14:10:26 -0400

    initial commit2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-console.log("My custom App.js is running!");
 import React from 'react';
-import Navbar from './components/Navbar/Navbar.js';
+import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Specials from './components/Specials/Specials';
 import WeeklyMenu from './components/WeeklyMenu/WeeklyMenu';
 import DayMenus from './components/DayMenus/DayMenus';
 import SnacksGallery from './components/SnacksGallery/SnacksGallery';
-import FAQAccordion from './components/FAQAccordion/FAQAccordion.js';
+import FAQAccordion from './components/FAQAccordion/FAQAccordion';
 import ContactGrid from './components/ContactGrid/ContactGrid';
 import Footer from './components/Footer/Footer';
 
@@ -15,24 +14,31 @@ function App() {
     <>
       <Navbar />
       <Hero />
+
       <section id="specials">
         <Specials />
       </section>
+
       <section id="weekly-menu">
         <WeeklyMenu />
       </section>
+
       <section id="day-menus">
         <DayMenus />
       </section>
+
       <section id="snacks-sweets">
         <SnacksGallery />
       </section>
+
       <section id="faqs">
         <FAQAccordion />
       </section>
+
       <section id="contact">
         <ContactGrid />
       </section>
+
       <Footer />
     </>
   );
